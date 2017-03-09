83cbdf1bd89b4cf6ba027d34477cc87ed3c287ed
Ludovic Claude <ludovic.claude54@gmail.com>
2017-03-09 19:28:14 +0100

    Docker push script

---
+++ docker_push.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -e
+
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     cd -P "$( dirname "$SOURCE" )"
+     pwd
+}
+
+export WORKSPACE=$(get_script_dir)
+
+if pgrep -lf sshuttle > /dev/null ; then
+  echo "sshuttle detected. Please close this program as it messes with networking and prevents builds inside Docker to work"
+  exit 1
+fi
+
+if groups $USER | grep &>/dev/null '\bdocker\b'; then
+  CAPTAIN="captain"
+else
+  CAPTAIN="sudo captain"
+fi
+
+#  WARNING: Requires captain 1.1.0 to push user tags
+$CAPTAIN push --branch-tags=false --commit-tags=true data-catalog-setup --tag $(git describe)
+sed "s/USER/${USER^}/" $WORKSPACE/slack.json > $WORKSPACE/.slack.json
+sed -i.bak "s/VERSION/$(git describe)/" $WORKSPACE/.slack.json
+curl -k -X POST --data-urlencode payload@$WORKSPACE/.slack.json https://hbps1.chuv.ch/slack/dev-activity
+rm -f $WORKSPACE/.slack.json
+rm -f $WORKSPACE/.slack.json.bak
